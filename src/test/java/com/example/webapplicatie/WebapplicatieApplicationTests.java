dd303f3d58a876c5606b7a64f7dba0a46d1418cf
Noortje <noortje_eliza@hotmail.com>
2022-04-07 11:25:06 +0200

    Initial commit

---
+++ src/test/java/com/example/webapplicatie/WebapplicatieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webapplicatie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebapplicatieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
